e480fa569093fb374abb77ad37c6b0b7ac509961
pripc <117996744+pripc@users.noreply.github.com>
2022-12-07 13:52:49 -0300

    Aprimorando a aplicação

--- DesafioTabuada/javascript.js
+++ DesafioTabuada/javascript.js
@@ -1,16 +1,21 @@
 let tabuada = window.document.getElementById('numTab')
 let quantValores= window.document.getElementById('qtdTab')
-listaTabuada = []
 let resultado = window.document.querySelector('p#res')
+
 function calcularTabuada(){
+    resultado.innerText = `A tabuada de ${tabuada.value} é: 
+    `
     if ((quantValores.value).length == 0 || (tabuada.value).lenth == 0){
         window.alert('[ERRO] Favor inserir valores nos campos')
     }else{
+        
         for (let i=1; i <=quantValores.value ; i++){
             let multiplicacao = tabuada.value * i
-            resultado.innerHTML = `${tabuada.value} x ${i}= ${multiplicacao}`
-        }
+            resultado.innerHTML += `${tabuada.value} x ${i}= ${multiplicacao} <br>`
+            }
         
-    }
+        }
+        tabuada.value = ''
+        quantValores.value =''
     
-}
+    }
